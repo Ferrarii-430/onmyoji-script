792d179ed5725b9593050be70e0d28c67f427dbe
CZY <906021948@qq.com>
2026-07-08 14:23:01 +0800

    [同步]

--- src/ui/ocrForm/ui_OcrForm.h
+++ src/ui/ocrForm/ui_OcrForm.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'OcrForm.ui'
 **
-** Created by: Qt User Interface Compiler version 6.2.4
+** Created by: Qt User Interface Compiler version 6.9.2
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
@@ -10,9 +10,6 @@
 #define UI_OCRFORM_H
 
 #include <QtCore/QVariant>
-#include <QtCore/QCoreApplication>
-#include <QtCore/QRect>
-#include <QtCore/QSize>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QCheckBox>
 #include <QtWidgets/QComboBox>
@@ -21,10 +18,8 @@
 #include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QLineEdit>
-#include <QtWidgets/QSizePolicy>
 #include <QtWidgets/QToolButton>
 #include <QtWidgets/QWidget>
-
 #include "src/ui/ocrForm/RoiPreviewWidget.h"
 
 QT_BEGIN_NAMESPACE
@@ -61,74 +56,74 @@ public:
     void setupUi(QWidget *OcrForm)
     {
         if (OcrForm->objectName().isEmpty())
-            OcrForm->setObjectName(QString::fromUtf8("OcrForm"));
+            OcrForm->setObjectName("OcrForm");
         OcrForm->resize(291, 560);
         formLayoutWidget = new QWidget(OcrForm);
-        formLayoutWidget->setObjectName(QString::fromUtf8("formLayoutWidget"));
+        formLayoutWidget->setObjectName("formLayoutWidget");
         formLayoutWidget->setGeometry(QRect(0, 0, 291, 554));
         formLayout = new QFormLayout(formLayoutWidget);
-        formLayout->setObjectName(QString::fromUtf8("formLayout"));
+        formLayout->setObjectName("formLayout");
         formLayout->setHorizontalSpacing(6);
         formLayout->setVerticalSpacing(20);
         formLayout->setContentsMargins(0, 0, 0, 0);
         label = new QLabel(formLayoutWidget);
-        label->setObjectName(QString::fromUtf8("label"));
+        label->setObjectName("label");
 
-        formLayout->setWidget(0, QFormLayout::LabelRole, label);
+        formLayout->setWidget(0, QFormLayout::ItemRole::LabelRole, label);
 
         lineTaskNameEdit = new QLineEdit(formLayoutWidget);
-        lineTaskNameEdit->setObjectName(QString::fromUtf8("lineTaskNameEdit"));
+        lineTaskNameEdit->setObjectName("lineTaskNameEdit");
 
-        formLayout->setWidget(0, QFormLayout::FieldRole, lineTaskNameEdit);
+        formLayout->setWidget(0, QFormLayout::ItemRole::FieldRole, lineTaskNameEdit);
 
         label_3 = new QLabel(formLayoutWidget);
-        label_3->setObjectName(QString::fromUtf8("label_3"));
+        label_3->setObjectName("label_3");
 
-        formLayout->setWidget(1, QFormLayout::LabelRole, label_3);
+        formLayout->setWidget(1, QFormLayout::ItemRole::LabelRole, label_3);
 
         ocrTextEdit = new QLineEdit(formLayoutWidget);
-        ocrTextEdit->setObjectName(QString::fromUtf8("ocrTextEdit"));
+        ocrTextEdit->setObjectName("ocrTextEdit");
 
-        formLayout->setWidget(1, QFormLayout::FieldRole, ocrTextEdit);
+        formLayout->setWidget(1, QFormLayout::ItemRole::FieldRole, ocrTextEdit);
 
         label_2 = new QLabel(formLayoutWidget);
-        label_2->setObjectName(QString::fromUtf8("label_2"));
+        label_2->setObjectName("label_2");
 
-        formLayout->setWidget(2, QFormLayout::LabelRole, label_2);
+        formLayout->setWidget(2, QFormLayout::ItemRole::LabelRole, label_2);
 
         roiPosLabel = new QLabel(formLayoutWidget);
-        roiPosLabel->setObjectName(QString::fromUtf8("roiPosLabel"));
+        roiPosLabel->setObjectName("roiPosLabel");
 
-        formLayout->setWidget(4, QFormLayout::LabelRole, roiPosLabel);
+        formLayout->setWidget(4, QFormLayout::ItemRole::LabelRole, roiPosLabel);
 
         roiPosLayout = new QHBoxLayout();
-        roiPosLayout->setObjectName(QString::fromUtf8("roiPosLayout"));
+        roiPosLayout->setObjectName("roiPosLayout");
         roiXBox = new QDoubleSpinBox(formLayoutWidget);
-        roiXBox->setObjectName(QString::fromUtf8("roiXBox"));
+        roiXBox->setObjectName("roiXBox");
         roiXBox->setMaximum(100.000000000000000);
         roiXBox->setDecimals(1);
 
         roiPosLayout->addWidget(roiXBox);
 
         roiYBox = new QDoubleSpinBox(formLayoutWidget);
-        roiYBox->setObjectName(QString::fromUtf8("roiYBox"));
+        roiYBox->setObjectName("roiYBox");
         roiYBox->setMaximum(100.000000000000000);
         roiYBox->setDecimals(1);
 
         roiPosLayout->addWidget(roiYBox);
 
 
-        formLayout->setLayout(4, QFormLayout::FieldRole, roiPosLayout);
+        formLayout->setLayout(4, QFormLayout::ItemRole::FieldRole, roiPosLayout);
 
         roiSizeLabel = new QLabel(formLayoutWidget);
-        roiSizeLabel->setObjectName(QString::fromUtf8("roiSizeLabel"));
+        roiSizeLabel->setObjectName("roiSizeLabel");
 
-        formLayout->setWidget(5, QFormLayout::LabelRole, roiSizeLabel);
+        formLayout->setWidget(5, QFormLayout::ItemRole::LabelRole, roiSizeLabel);
 
         roiSizeLayout = new QHBoxLayout();
-        roiSizeLayout->setObjectName(QString::fromUtf8("roiSizeLayout"));
+        roiSizeLayout->setObjectName("roiSizeLayout");
         roiWBox = new QDoubleSpinBox(formLayoutWidget);
-        roiWBox->setObjectName(QString::fromUtf8("roiWBox"));
+        roiWBox->setObjectName("roiWBox");
         roiWBox->setMaximum(100.000000000000000);
         roiWBox->setDecimals(1);
         roiWBox->setValue(100.000000000000000);
@@ -136,7 +131,7 @@ public:
         roiSizeLayout->addWidget(roiWBox);
 
         roiHBox = new QDoubleSpinBox(formLayoutWidget);
-        roiHBox->setObjectName(QString::fromUtf8("roiHBox"));
+        roiHBox->setObjectName("roiHBox");
         roiHBox->setMaximum(100.000000000000000);
         roiHBox->setDecimals(1);
         roiHBox->setValue(100.000000000000000);
@@ -144,61 +139,65 @@ public:
         roiSizeLayout->addWidget(roiHBox);
 
 
-        formLayout->setLayout(5, QFormLayout::FieldRole, roiSizeLayout);
+        formLayout->setLayout(5, QFormLayout::ItemRole::FieldRole, roiSizeLayout);
 
         label_4 = new QLabel(formLayoutWidget);
-        label_4->setObjectName(QString::fromUtf8("label_4"));
+        label_4->setObjectName("label_4");
 
-        formLayout->setWidget(6, QFormLayout::LabelRole, label_4);
+        formLayout->setWidget(6, QFormLayout::ItemRole::LabelRole, label_4);
 
         opencvErrorHandle = new QComboBox(formLayoutWidget);
-        opencvErrorHandle->setObjectName(QString::fromUtf8("opencvErrorHandle"));
+        opencvErrorHandle->setObjectName("opencvErrorHandle");
 
-        formLayout->setWidget(6, QFormLayout::FieldRole, opencvErrorHandle);
+        formLayout->setWidget(6, QFormLayout::ItemRole::FieldRole, opencvErrorHandle);
 
         stepInputLabel = new QLabel(formLayoutWidget);
-        stepInputLabel->setObjectName(QString::fromUtf8("stepInputLabel"));
+        stepInputLabel->setObjectName("stepInputLabel");
 
-        formLayout->setWidget(7, QFormLayout::LabelRole, stepInputLabel);
+        formLayout->setWidget(7, QFormLayout::ItemRole::LabelRole, stepInputLabel);
 
         stepInputBox = new QComboBox(formLayoutWidget);
-        stepInputBox->setObjectName(QString::fromUtf8("stepInputBox"));
+        stepInputBox->setObjectName("stepInputBox");
 
-        formLayout->setWidget(7, QFormLayout::FieldRole, stepInputBox);
+        formLayout->setWidget(7, QFormLayout::ItemRole::FieldRole, stepInputBox);
 
         label_6 = new QLabel(formLayoutWidget);
-        label_6->setObjectName(QString::fromUtf8("label_6"));
+        label_6->setObjectName("label_6");
 
-        formLayout->setWidget(3, QFormLayout::LabelRole, label_6);
+        formLayout->setWidget(3, QFormLayout::ItemRole::LabelRole, label_6);
 
         randomClickCheckBox = new QCheckBox(formLayoutWidget);
-        randomClickCheckBox->setObjectName(QString::fromUtf8("randomClickCheckBox"));
+        randomClickCheckBox->setObjectName("randomClickCheckBox");
 
-        formLayout->setWidget(3, QFormLayout::FieldRole, randomClickCheckBox);
+        formLayout->setWidget(3, QFormLayout::ItemRole::FieldRole, randomClickCheckBox);
 
         spinScoreBox = new QDoubleSpinBox(formLayoutWidget);
-        spinScoreBox->setObjectName(QString::fromUtf8("spinScoreBox"));
+        spinScoreBox->setObjectName("spinScoreBox");
         spinScoreBox->setMaximum(1.000000000000000);
         spinScoreBox->setSingleStep(0.010000000000000);
 
-        formLayout->setWidget(2, QFormLayout::FieldRole, spinScoreBox);
+        formLayout->setWidget(2, QFormLayout::ItemRole::FieldRole, spinScoreBox);
 
         label_5 = new QLabel(formLayoutWidget);
-        label_5->setObjectName(QString::fromUtf8("label_5"));
+        label_5->setObjectName("label_5");
 
-        formLayout->setWidget(8, QFormLayout::LabelRole, label_5);
+        formLayout->setWidget(8, QFormLayout::ItemRole::LabelRole, label_5);
 
         btnUploadImage = new QToolButton(formLayoutWidget);
-        btnUploadImage->setObjectName(QString::fromUtf8("btnUploadImage"));
+        btnUploadImage->setObjectName("btnUploadImage");
 
-        formLayout->setWidget(8, QFormLayout::FieldRole, btnUploadImage);
+        formLayout->setWidget(8, QFormLayout::ItemRole::FieldRole, btnUploadImage);
 
         roiPreview = new RoiPreviewWidget(formLayoutWidget);
-        roiPreview->setObjectName(QString::fromUtf8("roiPreview"));
+        roiPreview->setObjectName("roiPreview");
         roiPreview->setMinimumSize(QSize(260, 150));
-        roiPreview->setSizePolicy(QSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding));
+        QSizePolicy sizePolicy(QSizePolicy::Policy::Expanding, QSizePolicy::Policy::Expanding);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(roiPreview->sizePolicy().hasHeightForWidth());
+        roiPreview->setSizePolicy(sizePolicy);
 
-        formLayout->setWidget(9, QFormLayout::SpanningRole, roiPreview);
+        formLayout->setWidget(9, QFormLayout::ItemRole::SpanningRole, roiPreview);
 
 
         retranslateUi(OcrForm);
@@ -225,11 +224,11 @@ public:
         roiWBox->setSuffix(QCoreApplication::translate("OcrForm", "%", nullptr));
         roiHBox->setSuffix(QCoreApplication::translate("OcrForm", "%", nullptr));
         label_4->setText(QCoreApplication::translate("OcrForm", "\350\257\206\345\210\253\345\244\261\350\264\245\345\244\204\347\220\206", nullptr));
-        label_5->setText(QCoreApplication::translate("OcrForm", "\351\242\204\350\247\206", nullptr));
-        btnUploadImage->setText(QCoreApplication::translate("OcrForm", "\344\270\212\344\274\240\345\233\276\347\211\207\346\237\245\347\234\213\345\214\272\345\237\237", nullptr));
         stepInputLabel->setText(QCoreApplication::translate("OcrForm", "\350\267\263\350\275\254\346\255\245\351\252\244", nullptr));
         label_6->setText(QCoreApplication::translate("OcrForm", "\346\230\257\345\220\246\351\232\217\346\234\272\347\202\271\345\207\273", nullptr));
         randomClickCheckBox->setText(QString());
+        label_5->setText(QCoreApplication::translate("OcrForm", "\351\242\204\350\247\210", nullptr));
+        btnUploadImage->setText(QCoreApplication::translate("OcrForm", "\344\270\212\344\274\240\345\233\276\347\211\207\346\237\245\347\234\213\345\214\272\345\237\237", nullptr));
     } // retranslateUi
 
 };
